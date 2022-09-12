1170fcc6574bf498c14f894556b0ff72adabe390
per2 <kylerho@gmail.com>
2022-09-12 10:07:03 -0700

    wirewidgets

--- app/src/main/java/com/example/stopwatch/MainActivity.kt
+++ app/src/main/java/com/example/stopwatch/MainActivity.kt
@@ -3,16 +3,29 @@ package com.example.stopwatch
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
+import android.widget.Chronometer
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var storp : Button
+    lateinit var timer : Chronometer
+    lateinit var reset : Button
     //classwide statice constant in Kotlin
     companion object{
         val TAG = "MainActivity"
     }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        wirewidgets()
+    }
 
+    private fun wirewidgets() {
+        storp = findViewById(R.id.Button_main_start)
+        reset = findViewById(R.id.Button_main_reset)
+        timer = findViewById(R.id.chronometer_main_stopwatch)
     }
 
     override fun onStart() {
