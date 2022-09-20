1e4455de89aba372eba5a617efdc2fa7f8c3ea94
per2 <kylerho@gmail.com>
2022-09-20 10:06:49 -0700

    saved instance states

--- app/src/main/java/com/example/stopwatch/MainActivity.kt
+++ app/src/main/java/com/example/stopwatch/MainActivity.kt
@@ -2,24 +2,77 @@ package com.example.stopwatch
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.SystemClock
 import android.util.Log
 import android.widget.Button
 import android.widget.Chronometer
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var storp : Button
-    lateinit var timer : Chronometer
-    lateinit var reset : Button
+    private lateinit var storp : Button
+    private lateinit var timer : Chronometer
+    private lateinit var reset : Button
+    var isrunnning = false
+    var stopTime=0L
     //classwide statice constant in Kotlin
     companion object{
         val TAG = "MainActivity"
+        val STATE_TIME = "display time"
+        val ISRUNNING = "is running"
+        val BASE = "timer base"
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         wirewidgets()
+        if(savedInstanceState!=null){
+            isrunnning = savedInstanceState.getBoolean(ISRUNNING)
+            timer.base = savedInstanceState.getLong(BASE)
+            stopTime = savedInstanceState.getLong(STATE_TIME)
+            //if(!isrunnning)
+//            timer.base = SystemClock.elapsedRealtime()-(stopTime-timer.base)
+        }
+        if(savedInstanceState==null) {
+            stopstart()
+            stopstart()
+        }
+        storp.setOnClickListener {
+            stopstart()
+        }
+        reset.setOnClickListener {
+            timer.setBase(SystemClock.elapsedRealtime())
+            if(storp.text=="start")
+                isrunnning = !isrunnning
+            stopstart()
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        if(!isrunnning)
+        stopTime = SystemClock.elapsedRealtime()
+        outState.putLong(BASE, timer.base)
+        outState.putLong(STATE_TIME, stopTime)
+        outState.putBoolean(ISRUNNING, isrunnning)
+        super.onSaveInstanceState(outState)
+
+    }
+
+    private fun stopstart() {
+        if(!isrunnning){
+            if(stopTime!=0L)
+                timer.base = SystemClock.elapsedRealtime()-(stopTime-timer.base)
+            timer.start()
+            storp.text="stop"
+
+        }
+        else{
+            timer.stop()
+            storp.text="start"
+            stopTime=SystemClock.elapsedRealtime()
+
+        }
+        isrunnning = !isrunnning
     }
 
     private fun wirewidgets() {
